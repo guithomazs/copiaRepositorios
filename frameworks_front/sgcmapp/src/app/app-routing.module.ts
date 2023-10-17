4caede86c889f6559e146472513d8ec61b2f6617
kleicezar <140003646+kleicezar@users.noreply.github.com>
2023-10-17 19:27:46 -0300

    a foda-se

--- frameworks_front/sgcmapp/src/app/app-routing.module.ts
+++ frameworks_front/sgcmapp/src/app/app-routing.module.ts
@@ -7,15 +7,31 @@ import { ProfissionaisListComponent } from './component/profissionais/profission
 import { ProfissionaisFormComponent } from './component/profissionais/profissionais-form/profissionais-form.component';
 import { EspecialidadeListComponent } from './component/especialidade/especialidade-list/especialidade-list.component';
 import { EspecialidadeFormComponent } from './component/especialidade/especialidade-form/especialidade-form.component';
+import { PacientesFormComponent } from './component/pacientes/pacientes-form/pacientes-form.component';
+import { PacientesListComponent } from './component/pacientes/pacientes-list/pacientes-list.component';
+import { ConveniosListComponent } from './component/convenios/convenios-list/convenios-list.component';
+import { ConveniosFormComponent } from './component/convenios/convenios-form/convenios-form.component';
+import { UnidadesFormComponent } from './component/unidades/unidades-form/unidades-form.component';
+import { UnidadesListComponent } from './component/unidades/unidades-list/unidades-list.component';
+import { UsuariosFormComponent } from './component/usuarios/usuarios-form/usuarios-form.component';
+import { UsuariosListComponent } from './component/usuarios/usuarios-list/usuarios-list.component';
 
 const routes: Routes = [
   { path: 'agenda', component: AgendaListComponent },
   { path: 'agenda/form', component: AgendaFormComponent },
   { path: 'atendimento', component: AtendimentoListComponent },
+  { path: 'pacientes/form', component: PacientesFormComponent },
+  { path: 'pacientes', component: PacientesListComponent },
   { path: 'profissionais/form', component: ProfissionaisFormComponent },
   { path: 'profissionais', component: ProfissionaisListComponent },
+  { path: 'convenios/form', component: ConveniosFormComponent },
+  { path: 'convenios', component: ConveniosListComponent },
   { path: 'config/especialidades/form', component: EspecialidadeFormComponent},
   { path: 'config/especialidades', component: EspecialidadeListComponent},
+  { path: 'config/unidades/form', component: UnidadesFormComponent},
+  { path: 'config/unidades', component: UnidadesListComponent},
+  { path: 'config/usuarios/form', component: UsuariosFormComponent},
+  { path: 'config/usuarios', component: UsuariosListComponent},
 ];
 
 @NgModule({
--- frameworks_front/sgcmapp/src/app/component/pacientes/pacientes-list/pacientes-list.component.ts
+++ frameworks_front/sgcmapp/src/app/component/pacientes/pacientes-list/pacientes-list.component.ts
@@ -1,10 +1,31 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { IList } from '../../i-list';
+import { Paciente } from '../../model/paciente';
+import { PacienteService } from 'src/app/service/paciente.service';
 
 @Component({
   selector: 'app-pacientes-list',
   templateUrl: './pacientes-list.component.html',
   styleUrls: ['./pacientes-list.component.css']
 })
-export class PacientesListComponent {
+export class PacientesListComponent implements IList<Paciente>, OnInit{
+  constructor(private servico: PacienteService){}
+
+  ngOnInit(): void {
+    this.get();
+  }
+
+  registros: Paciente[] = Array<Paciente>();
+  
+  get(termoBusca?: string | undefined): void {
+    this.servico.get(termoBusca).subscribe({
+      next: (resposta: Paciente[]) => {
+        this.registros = resposta;
+      }
+    });
+  }
+  delete(id: number): void {
+    this.servico.delete(id).subscribe({});
+  }
 
 }
